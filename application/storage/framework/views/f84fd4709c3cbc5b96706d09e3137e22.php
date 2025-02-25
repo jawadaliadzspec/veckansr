9e72af198882b94ae4b3ae48d2f2605528662de1
root <root@ip-172-31-41-218.eu-north-1.compute.internal>
2025-02-25 06:11:41 +0000

    deleted cache

--- application/storage/framework/views/f84fd4709c3cbc5b96706d09e3137e22.php
+++
@@ -1,3 +0,0 @@
-<?php echo  loadExtension('tawk-chat') ?>
-<?php echo  loadExtension('google-analytics') ?>
-<?php /**PATH /var/www/html/veckansr/application/resources/views/includes/plugins.blade.php ENDPATH**/ ?>
