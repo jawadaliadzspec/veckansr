e24f9d85e964e0bc6162c36f635dc3b29b6ca4e7
Jawad Ali <jawad.ali@adzspec.com>
2025-05-12 11:44:00 +0500

    deal card fixes

--- application/resources/views/presets/default/couponFilter/exclusive_coupon_search.blade.php
+++ application/resources/views/presets/default/couponFilter/exclusive_coupon_search.blade.php
@@ -1,46 +1,65 @@
 <div class="row gy-4">
     @forelse($exclusiveCoupons as $item)
-    <div class="col-lg-4 col-md-6">
-        <div class="coupon-card wow animate__animated animate__fadeInUp" data-wow-delay="0.5s">
-            <div class="card-ribbon-wrap">
-                <button class="fav-cta addToWishList" data-id="{{ $item->id }}">
-                    @if (auth()->check() && $item->wishlists->count() > 0)
-                        <i class="fas fa-heart"></i>
-                    @else
-                        <i class="far fa-heart"></i>
+        <div class="col-lg-4 col-md-6">
+            <div class="coupon-card wow animate__animated animate__fadeInUp" data-wow-delay="0.5s">
+                <div class="card-ribbon-wrap">
+                    <button class="fav-cta addToWishList" data-id="{{ $item->id }}">
+                        @if (auth()->check() && $item->wishlists->count() > 0)
+                            <i class="fas fa-heart"></i>
+                        @else
+                            <i class="far fa-heart"></i>
+                        @endif
+                    </button>
+                    @php
+                        $image = @$item->store->image;
+                        $isUrl = filter_var($image, FILTER_VALIDATE_URL);
+                    @endphp
+                    <div class="ex-cta">
+                        <img src="{{ $isUrl ? $image : getImage(getFilePath('store') . '/' . $image) }}" height="30px" alt="@lang('Store Image')">
+                        {{--                                    <i class="fas fa-gem"></i>--}}
+                        {{--                                    @if($item->is_featured == 1)--}}
+                        {{--                                    <p>@lang('Featured')</p>--}}
+                        {{--                                    @elseif($item->is_exclusive == 1)--}}
+                        {{--                                    <p>@lang('Exclusive')</p>--}}
+                        {{--                                    @else--}}
+                        {{--                                    <p>@lang('New')</p>--}}
+                        {{--                                    @endif--}}
+
+                    </div>
+                </div>
+                <div class="card-thumb">
+                    <img src="{{ $item->thumnail }}" height="90px" alt="@lang('Store Image')">
+                </div>
+                <div class="card-content-wrap">
+                    <p class="card-title">{{__($item->title)}}</p>
+                    {{--                                <a href="javascript:void(0)" class="btn btn--base w-100 getCoupon" data-id="{{$item->id}}" data-title="{{$item->title}}" data-code="{{$item->code}}" data-description="{{$item->description}}" data-link="{{$item->link}}">@lang('Get Code')</a>--}}
+                    @if($item->productPrice == $item->oldPrice)
+                        <div class="ex-cta">
+                            {{$item->productPrice}}
+                        </div>
                     @endif
-                </button>
-                <div class="ex-cta">
-                    <i class="fas fa-gem"></i>
-                    <p>@lang('Exclusive')</p>
+                    @if(!$item->is_deal)
+                        <a href="javascript:void(0)" class="btn btn--base w-100 getCoupon1"
+                           data-id="{{ $item->id }}" data-title="{{ $item->title }}"
+                           data-code="{{ $item->code }}" data-description="{{ $item->description }}"
+                           data-link="{{ $item->link }}">
+                            @lang('Get Code')
+                        </a>
+                    @else
+                        @if($item->productPrice == $item->oldPrice)
+                            <a target="_blank" class="btn btn--base w-100" href="{{ $item->link }}">
+                                @lang('Buy')
+                            </a>
+                        @else
+                            <a class="btn btn--base w-100" href="{{ route('couponDetails', $item->path) }}">
+                                @lang('Show Details')
+                            </a>
+                        @endif
+                    @endif
+                    @if($item->expire_date)<p class="card-action">{!! isExpired($item->id) !!}</p>@endif
                 </div>
             </div>
-            @php
-                $image = @$item->store->image;
-                $isUrl = filter_var($image, FILTER_VALIDATE_URL);
-            @endphp
-            <div class="card-thumb">
-                <img src="{{ $isUrl ? $image : getImage(getFilePath('store') . '/' . $image) }}" height="90px" alt="@lang('Store Image')">
-            </div>
-            <div class="card-content-wrap">
-                <p class="card-title">{{__($item->title)}}</p>
-{{--                <a href="javascript:void(0)" class="btn btn--base w-100 getCoupon" data-id="{{$item->id}}" data-title="{{$item->title}}" data-code="{{$item->code}}" data-description="{{$item->description}}" data-link="{{$item->link}}">@lang('Get Code')</a>--}}
-                @if(!$item->is_deal)
-                    <a href="javascript:void(0)" class="btn btn--base w-100 getCoupon1"
-                       data-id="{{ $item->id }}" data-title="{{ $item->title }}"
-                       data-code="{{ $item->code }}" data-description="{{ $item->description }}"
-                       data-link="{{ $item->link }}">
-                        @lang('Get Code')
-                    </a>
-                @else
-                    <a class="btn btn--base w-100" href="{{ route('couponDetails', $item->path) }}">
-                        @lang('Show Details')
-                    </a>
-                @endif
-                @if($item->expire_date)<p class="card-action">{!! isExpired($item->id) !!}</p>@endif
-            </div>
         </div>
-    </div>
     @empty
     <p class="text-center h4">{{__($emptyMessage)}}</p>
     @endforelse
